3ee33e21ae000348dd7c6aa143955ad16640f34f
tom <tom@tomdeMacBook-Pro.local>
2022-01-07 22:54:22 +0800

    测试

--- src/modules/build/build.service.ts
+++ src/modules/build/build.service.ts
@@ -49,7 +49,7 @@ export class BuildService {
         execSync('npm run build');
         cp('-rf', 'dist', '../dist');
         cd('..');
-        // rm('-rf', git_project_name);
+        rm('-rf', git_project_name);
         // 更改端口号
         const wwwData = readFileSync('bin/www', 'utf-8').split(/\r\n|\n|\r/gm);
         wwwData.splice(
@@ -194,7 +194,7 @@ export class BuildService {
         cd(data.git_project_name);
         execSync('npm i');
         execSync('npm run build');
-        mv('-f', 'dist', '../dist');
+        cp('-f', 'dist', '../dist');
         cd('..');
         rm('-rf', data.git_project_name);
       }
